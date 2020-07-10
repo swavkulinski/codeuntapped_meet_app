1661025474103ff5ee9d53842b4dc07729103c0a
Robotoaster <swav.kulinski@gmail.com>
2020-07-10 09:57:59 +0100

    data classes

---
+++ lib/data.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+
+class Attendee {
+  final String firstName;
+  final String lastName;
+  const Attendee({@required this.firstName,@required this.lastName});
+}
+
+const attendees = [
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  Attendee(firstName:'Swav',lastName:'Kulinski'),
+  ];
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,38 +27,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Hero(
+        tag: 'main',
+        child: Image.asset('assets/cu_logo.png'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
     );
   }
 }
